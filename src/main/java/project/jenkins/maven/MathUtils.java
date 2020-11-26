70883efd5fbd054103a3a8c88bdcffa2edce6952
Pavani Ganisetti <pavaniganisetti9@gmail.com>
2020-11-26 16:26:46 +0530

    adding project files

---
+++ src/main/java/project/jenkins/maven/MathUtils.java
@@ -0,0 +1,12 @@
+package project.jenkins.maven;
+
+public class MathUtils {
+	public static void main(String args[])
+	{
+		System.out.println("Hello!");
+	}
+	public int add(int a,int b)
+	{
+		return a+b;
+	}
+}
---
+++ src/test/java/project/jenkins/maven/MathUtilsTest.java
@@ -0,0 +1,25 @@
+package project.jenkins.maven;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	private MathUtils mathUtils;
+	
+	@BeforeEach
+	void initEach()
+	{
+		mathUtils = new MathUtils();
+	}
+	
+	@Test
+	@DisplayName("Addition")
+	void testAdd() {
+		assertEquals(2,mathUtils.add(1, 1),()-> "Should return correct sum");
+	}
+
+}
